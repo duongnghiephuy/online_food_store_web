c6cfde2af99c55b2b537b45ae8ba1e4298515405
duongnghiephuy <huydragon23@gmail.com>
2021-02-22 16:57:41 +0900

    "Add menu""

--- menu.php
+++ menu.php
@@ -1,6 +1,40 @@
 <?php
 include("navbar.php")
 ?>
+<section class="nest-grid">
+    <figure>
+        <img src="https://bizweb.dktcdn.net/100/347/970/products/bachi2.png?v=1555560138907">
+        <figcaption>
+            Thịt ba chỉ
+        </figcaption>
+    </figure>
+    <figure>
+        <img src="https://monngondongian.com/wp-content/uploads/2019/02/suon-non-1.jpg">
+        <figcaption>
+            Sườn heo
+        </figcaption>
+    </figure>
+    <figure>
+        <img
+            src="https://cdn.tgdd.vn/Files/2018/11/28/1134436/nuoc-dua-de-duoc-bao-lau-sau-mot-ngay-co-nen-uong-khong--1.jpg">
+        <figcaption>
+            Dừa non
+        </figcaption>
+    </figure>
+    <figure>
+        <img src="https://login.medlatec.vn//ImagePath/images/20200602/20200602_cong-dung-cua-toi-1.jpg">
+        <figcaption>
+            Tỏi
+        </figcaption>
+    </figure>
+    <figure>
+        <img src="https://media-cdn.laodong.vn/Storage/NewsPortal/Uploaded/phamthuhien/2015_05_25/mig29_PUUT.jpg">
+        <figcaption>
+            Nghệ
+        </figcaption>
+    </figure>
+
+</section>
 </div>
 <footer>
     Hai Bà Trưng, Hà Nội
